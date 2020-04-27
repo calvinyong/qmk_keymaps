5a70ce22c8fce926618269b4492004d3b3323ea1
Calvin Yong <calvinpyong@gmail.com>
2020-04-27 16:58:43 -0400

    ok

--- kyria/calvin/keymap.c
+++ kyria/calvin/keymap.c
@@ -20,19 +20,47 @@ enum layers {
     _LOWER,
     _RAISE,
     _ADJUST,
-    _ARROWS,
-    _NUMPAD,
-    _MOUSE
+    _EXTRA,
+    _NUMPAD
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_COLEMAK] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_BSPC, KC_LCTL, KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_ESC, KC_PSCR, KC_RALT, TG(5), KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_ESC, KC_LALT, MO(1), KC_SPC, KC_LGUI, KC_LGUI, LT(4,KC_ENT), MO(2), MO(6), KC_RCTL),
-    [_LOWER] = LAYOUT(KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS, KC_DEL, KC_NO, KC_INS, KC_HOME, KC_PGUP, KC_NO, KC_NO, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_NO, KC_NO, KC_DEL, KC_END, KC_PGDN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO),
-    [_RAISE] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS, KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_NO, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_NO, KC_NO, KC_NO, KC_NO, KC_F12, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO),
-    [_ADJUST] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_TOG, RGB_VAI, RGB_HUI, RGB_SAI, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_MOD, RGB_VAD, RGB_HUD, RGB_SAD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-    [_ARROWS] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-    [_NUMPAD] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_PPLS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_4, KC_5, KC_6, KC_COMM, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_1, KC_2, KC_3, KC_DOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_0, KC_NO),
-    [_MOUSE] = LAYOUT(KC_NO, KC_NO, KC_BTN1, KC_MS_U, KC_BTN3, KC_WH_U, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO)
+    [_COLEMAK] = LAYOUT(
+         KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B,                                                          KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_BSPC,
+        KC_LCTL, KC_A, KC_R, KC_S, KC_T, KC_G,                                                          KC_M, KC_N, KC_E, KC_I, KC_O, KC_QUOT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_D, KC_V,     KC_ESC, KC_PSCR, TG(_NUMPAD), KC_LALT,               KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                 XXXXXXX, XXXXXXX, MO(_LOWER),     KC_SPC, XXXXXXX, XXXXXXX, LT(_EXTRA,KC_ENT),         MO(_RAISE), KC_LGUI, KC_RCTL
+    ),
+    [_LOWER] = LAYOUT(
+        KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                                             KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+         KC_DEL, _______, _______, _______, _______, _______,                                             _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+        _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______,     _______, KC_PSCR, _______, KC_LBRC, KC_RBRC, _______,
+                                   _______, _______, _______,     _______, _______, _______, _______,     _______, _______, _______
+    ),
+    [_RAISE] = LAYOUT(
+         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,                                                              KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
+         KC_DEL, _______, _______, _______, _______, _______,                                               _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
+        _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______,      _______, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY,
+                                   _______, _______, _______,      _______, _______, _______, _______,      _______, _______, _______
+    ),
+    [_ADJUST] = LAYOUT(
+        _______,    KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                                                 KC_F6,   KC_F7,    KC_F8,   KC_F9,  KC_F10, _______,
+        _______, _______,  KC_INS, KC_HOME, KC_PGUP, _______,                                               _______, RGB_TOG,  RGB_VAI,  KC_F11,  KC_F12, _______,
+        _______, _______,  KC_DEL,  KC_END, KC_PGDN, _______,       _______, _______, _______, _______,     _______, RGB_M_R, RGB_M_SW, RGB_M_G, _______, _______,
+                                   _______, _______, _______,       _______, _______, _______, _______,     _______, _______, _______
+    ),
+    [_EXTRA] = LAYOUT(
+        _______, G(KC_Q), G(KC_W), G(KC_F), G(KC_P), G(KC_B),                                         G(KC_M), G(KC_N), G(KC_E), G(KC_I), _______, _______,
+        _______, G(KC_A), G(KC_R), G(KC_S), G(KC_T), _______,                                         KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,   G(KC_O), _______,
+        _______, _______, _______, A(KC_C), _______, _______,   _______, _______, _______, _______,   G(KC_K), G(KC_H), _______, _______, _______, _______,
+                                   _______, _______, _______,   G(KC_SPC), _______, _______, _______,   _______, _______, _______
+    ),
+    [_NUMPAD] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                                               _______, KC_7, KC_8, KC_9, KC_PPLS, _______,
+        _______, _______, _______, _______, _______, _______,                                               _______, KC_4, KC_5, KC_6, KC_COMM, _______,
+        _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______,        KC_0, KC_1, KC_2, KC_3, KC_DOT, _______,
+                                   _______, _______, _______,       _______, _______, _______, _______,     _______, _______, _______
+    ),
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -70,7 +98,7 @@ static void render_qmk_logo(void) {
 static void render_status(void) {
     // QMK Logo and version information
     render_qmk_logo();
-    oled_write_P(PSTR("Kyria rev1.0\n\n"), false);
+    oled_write_P(PSTR("Calvin's Kyria\n\n"), false);
 
     // Host Keyboard Layer Status
     oled_write_P(PSTR("Layer: "), false);
@@ -87,14 +115,11 @@ static void render_status(void) {
         case _ADJUST:
             oled_write_P(PSTR("Adjust\n"), false);
             break;
-        case _ARROWS:
-            oled_write_P(PSTR("Arrows\n"), false);
+        case _EXTRA:
+            oled_write_P(PSTR("Extra\n"), false);
             break;
         case _NUMPAD:
             oled_write_P(PSTR("Numpad\n"), false);
-            break;
-        case _MOUSE:
-            oled_write_P(PSTR("Mouse\n"), false);
             break;
         default:
             oled_write_P(PSTR("Undefined\n"), false);
